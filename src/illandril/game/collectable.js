8745d5096c7b5c6889e6834217e6fa266965c0c7
Illandril <git@illandril.net>
2010-11-13 18:33:30 -0500

    initial attempt at multiple types of game objects

--- src/illandril/game/collectable.js
+++ src/illandril/game/collectable.js
@@ -4,11 +4,12 @@ goog.require("illandril.game.GameObject");
 
 /**
  * @constructor
+ * @extends illandril.game.GameObject
  */
 illandril.game.Collectable = function( world, bounds, bg ) {
   illandril.game.GameObject.call( this, world, bounds, bg );
 };
-goog.inherits( illandril.game.GameObject, illandril.game.Collectable );
+goog.inherits( illandril.game.Collectable, illandril.game.GameObject );
 
 illandril.game.Collectable.prototype.blocks = function( otherObject ) {
   return false;
