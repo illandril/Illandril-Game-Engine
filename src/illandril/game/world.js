508925b25b2f53dff88a1e060bea1ffb51720bbb
Illandril <git@illandril.net>
2010-11-13 15:15:01 -0500

    sync testing

--- src/illandril/game/world.js
+++ src/illandril/game/world.js
@@ -15,7 +15,7 @@ illandril.game.World = function() {
   this.buckets = {};
 };
 
-var bucketSize = 50;
+var bucketSize = 100;
 illandril.game.World.prototype.getBucket = function( center ) {
   var bucketX = Math.round( center.x / bucketSize );
   if ( this.buckets[bucketX] == null ) {
