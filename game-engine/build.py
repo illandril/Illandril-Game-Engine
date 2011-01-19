b4f187cf6d07924c2bcdd8615669894c31724955
Illandril <git@illandril.net>
2011-01-19 09:54:03 -0600

    Tweaks to builder

--- game-engine/build.py
+++ game-engine/build.py
@@ -1,5 +1,7 @@
 import os,sys
 
+debug = True
+
 builder = "../closure-library/closure/bin/build/closurebuilder.py"
 args = " --root=../closure-library/"
 args = args + " --root=src/"
@@ -7,9 +9,17 @@ args = args + " --namespace=illandril.game.Engine"
 args = args + " --namespace=illandril"
 args = args + " --output_mode=compiled"
 args = args + " --compiler_jar=../closure-compiler/compiler.jar"
-args = args + " --compiler_flags=--compilation_level=SIMPLE_OPTIMIZATIONS"
-#args = args + " --compiler_flags=--compilation_level=ADVANCED_OPTIMIZATIONS"
 args = args + " --compiler_flags=--formatting=PRETTY_PRINT"
+
+if debug:
+  args = args + " --compiler_flags=--compilation_level=SIMPLE_OPTIMIZATIONS"
+else:
+  args = args + " --compiler_flags=--compilation_level=ADVANCED_OPTIMIZATIONS"
+  
+  # Disable debugging
+  args = args + " --compiler_flags=--define='illandril.DEBUG=false'"
+  args = args + " --compiler_flags=--define='goog.DEBUG=false'"
+
 os.system( sys.executable + " " + builder + args + " > bin/engine.js" )
 
 def readfile( filename, output ):
