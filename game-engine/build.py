2baa9ce35fe6aa1bd79dde2091128fab1e08d222
Illandril <git@illandril.net>
2011-08-12 10:49:26 -0500

    Added builder

---
+++ game-engine/build.py
@@ -0,0 +1,44 @@
+import os,sys
+
+if ( len( sys.argv ) < 2 ):
+  print "Usage: build.py name namespace [namespace]...";
+  sys.exit(2);
+ 
+debug = True
+
+builder = "../closure-library/closure/bin/build/closurebuilder.py"
+args = " --root=../closure-library/"
+args = args + " --root=../box2d/illandril/"
+args = args + " --root=src/"
+for arg in sys.argv[2:]:
+  args = args + " --namespace=" + arg
+args = args + " --output_mode=compiled"
+args = args + " --compiler_jar=../closure-compiler/compiler.jar"
+args = args + " --compiler_flags=--formatting=PRETTY_PRINT"
+
+if debug:
+  args = args + " --compiler_flags=--compilation_level=SIMPLE_OPTIMIZATIONS"
+else:
+  args = args + " --compiler_flags=--compilation_level=ADVANCED_OPTIMIZATIONS"
+  
+  # Disable debugging
+  args = args + " --compiler_flags=--define='illandril.DEBUG=false'"
+  args = args + " --compiler_flags=--define='goog.DEBUG=false'"
+
+os.system( sys.executable + " " + builder + args + " > bin/" + sys.argv[1] + ".js" )
+
+def readfile( filename, output ):
+	f = file(filename)
+	while True:
+		line = f.readline()
+		if len(line) == 0:
+			break
+		output.write(line)
+	f.close()
+
+out = file("bin/engine.css", "w")
+for subdir, dirs, files in os.walk("styles/"):
+	for filename in files:
+		if filename.endswith( ".css" ):
+			readfile( "styles/" + filename, out )
+out.close()
